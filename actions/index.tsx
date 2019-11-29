9d9f725762bc6ab684ce8c6e23f585c3e109bdbd
paddy-7385 <pandurangmatkar4@gmail.com>
2019-11-29 13:36:48 +0530

    refactor

--- actions/index.tsx
+++ actions/index.tsx
@@ -2,19 +2,19 @@ export function addTodo(text: string) {
   return {
     type: "ADD_TODO",
     text: text
-  }
+  };
 }
 
 export function toggleTodo(text: string) {
   return {
     type: "TOGGLE_TODO",
     text: text
-  }
+  };
 }
 
 export function setFilter(text: string) {
   return {
-    type: "SET_FILTER", 
+    type: "SET_FILTER",
     visibility: text
-  }
+  };
 }
--- index.tsx
+++ index.tsx
@@ -1,17 +1,17 @@
-import {createStore, combineReducers} from 'redux';
-import todosReducer from './reducers/todosReducer';
-import filterReducer from './reducers/filterReducer';
+import { createStore, combineReducers } from "redux";
+import todosReducer from "./reducers/todosReducer";
+import filterReducer from "./reducers/filterReducer";
 
-import {addTodo,toggleTodo,setFilter} from './actions';
+import { addTodo, toggleTodo, setFilter } from "./actions";
 
-let store = createStore(combineReducers({todos:todosReducer,filter:filterReducer}));
+let store = createStore(
+  combineReducers({ todos: todosReducer, filter: filterReducer })
+);
 
-store.subscribe(()=> console.log(store.getState()));
+store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(addTodo("Onion"));
 store.dispatch(addTodo("Milk"));
 store.dispatch(addTodo("Cheese"));
 store.dispatch(toggleTodo("Milk"));
 store.dispatch(setFilter("SHOW_COMPLETED"));
-
-
